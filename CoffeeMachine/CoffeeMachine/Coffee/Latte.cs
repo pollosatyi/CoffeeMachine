e5c5a962d46244626447e3f3c1cf69f56219de79
pollosatyi <pollosatyi@mail.ru>
2024-12-08 17:15:00 +0300

    доработка

--- CoffeeMachine/CoffeeMachine/Coffee/Latte.cs
+++ CoffeeMachine/CoffeeMachine/Coffee/Latte.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeMachine.Products;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace CoffeeMachine.Coffee
 {
-    internal class Latte
+    public class Latte : Coffee
     {
+        private string _name;
+        private double _amountOfCoffee;
+        private CoffeeBeans _coffeeBeans;
+        private Milk _milk;
+        private Water _water;
+        private Sugar _sugar;
+
+
+        public Latte(CoffeeBeans coffeeBeans ,Milk milk, Water water, Sugar sugar)
+        {
+            _milk = milk;
+            _water = water;
+            _sugar = sugar;
+            _coffeeBeans = coffeeBeans;
+        }
+        public override void CreateCoffee()
+        {
+            _amountOfCoffee = _coffeeBeans.AmountOfBeans * 10 + _milk.AmountOfMilk * 60 + _water.AmountOfWater * 28 + _sugar.AmountOfSugar * 2;
+        }
+
+        public override void PrintCoffee()
+        {
+            Console.WriteLine($"Кофе {_name} {_amountOfCoffee} мл готов");
+        }
     }
 }
